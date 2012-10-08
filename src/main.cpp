4e57bd283c6117b118bb32a02d6309f89cf39d45
Sam Oates <sam@samoatesgames.com>
2012-10-08 10:55:57 +0000

    Initial Commit.  - Enabled warnings as errors in debug builds.  - Removed all additional debugging information from release.

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+
+#include "main.h"
+
+/*!
+ * \brief Main entry point of the application
+ * \return 0 on success, else an error code
+ */
+int WINAPI WinMain(
+	HINSTANCE hInstance,				//!< A handle to the current instance of the application.
+	HINSTANCE hPrevInstance,			//!< A handle to the previous instance of the application. This parameter is always NULL.
+	LPSTR cmdline,						//!< Command line arguments
+	int cmdshow							//!< The initial show status of the application
+	)
+{
+
+	return 0;
+}
