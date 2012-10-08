00e0e58f1712e878c7c1cfa659a5ce127cd9d295
samoatesgames <samoatesgames@0aab44fd-6b68-406f-af70-1c002fc39f85>
2012-10-08 10:57:17 +0000

    Added VisCraft main class.  - VisCraft will create a window, loop in a run loop, then shutdown.  - Added helper commands to safely delete memory.  - Added advanced memory leek detection in debug builds.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 /*!
  * \brief Main entry point of the application
- * \return 0 on success, else an error code
+ * \return TRUE on success, FALSE on unknown error else an error code
  */
 int WINAPI WinMain(
 	HINSTANCE hInstance,				//!< A handle to the current instance of the application.
@@ -12,6 +12,18 @@ int WINAPI WinMain(
 	int cmdshow							//!< The initial show status of the application
 	)
 {
+	#if defined(_DEBUG)
+		_CrtSetDbgFlag(_CrtSetDbgFlag(_CRTDBG_REPORT_FLAG)|_CRTDBG_LEAK_CHECK_DF);
+	#endif
 
-	return 0;
+	VisCraft *const visCraft = new VisCraft();
+	if (!visCraft->Create())
+		return FALSE;
+
+	visCraft->Run();
+	visCraft->Release();
+
+	SafeDelete(visCraft);
+
+	return TRUE;
 }
--- src/main.h
+++ src/main.h
@@ -2,7 +2,40 @@
 	Header file includes
 */
 #include <windows.h>
+#include "viscraft.h"
 
 /**
 	Additionally included libraries
 */
+
+
+/**
+	Function prototypes helpers
+*/
+
+template<typename T> 
+void SafeDelete(T*& ptr) {
+	if (ptr) 
+	{
+		delete ptr;
+		ptr = NULL;
+	}
+}
+
+template<typename T> 
+void SafeDelete(T*const& ptr) {
+	if (ptr) 
+	{
+		delete ptr;
+		const_cast<T*>(ptr) = NULL;
+	}
+}
+
+/**
+	Advanced memory leek detection
+*/
+#if defined(_DEBUG)
+	#include <crtdbg.h>
+	#define new new( _NORMAL_BLOCK, __FILE__, __LINE__)
+	#pragma warning(disable : 4345)
+#endif
