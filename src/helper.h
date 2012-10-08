14ef515592b836acc5f8492ab5bd6d8d835973a9
samoatesgames <samoatesgames@0aab44fd-6b68-406f-af70-1c002fc39f85>
2012-10-08 11:04:20 +0000

    Added helper header.  - Includes custom assert (ASSERT) which takes the condition and a message, to help explain what an assert implies.  - Includes SafeDelete/release/destroywindow methods, which free memory safely. nulling the pointer.

---
+++ src/helper.h
@@ -0,0 +1,69 @@
+#pragma once
+
+/**
+	Header file includes
+*/
+
+#include <windows.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+/**
+	Custom Assert
+*/
+
+#ifndef NDEBUG
+	#define ASSERT(condition, message) \
+	do { \
+		if (!(condition)) { \
+			std::stringstream msg; \
+			msg << "Assert Failed: \"" #condition "\"\n In " << __FILE__ \
+				<< "(" << __LINE__ << ")\n \"" << message << "\"" << std::endl; \
+			MessageBox(NULL, msg.str().c_str(), "Assert Failed", MB_OK | MB_ICONERROR); \
+		} \
+	} while (false)
+#else
+	#define ASSERT(condition, message) do { } while (false)
+#endif
+
+
+/**
+	Function prototypes helpers
+*/
+
+/*!
+ * \brief delete memory and null the pointer
+ */
+template<typename T> 
+void SafeDelete(T*& ptr) {
+	if (ptr) 
+	{
+		delete ptr;
+		ptr = nullptr;
+	}
+}
+
+/*!
+ * \brief destroy a window and null the pointer
+ */
+template<typename T> 
+void SafeDestroyWindow(T*& ptr) {
+	if (ptr) 
+	{
+		DestroyWindow(ptr);
+		ptr = nullptr;
+	}
+}
+
+/*!
+ * \brief release and null a pointer
+ */
+template<typename T> 
+void SafeRelease(T*& ptr) {
+	if (ptr) 
+	{
+		ptr->Release();
+		ptr = nullptr;
+	}
+}
