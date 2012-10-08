6b22db5e370a8c95da2f8bce484268a1a6a0b496
samoatesgames <samoatesgames@0aab44fd-6b68-406f-af70-1c002fc39f85>
2012-10-08 11:08:08 +0000

    Updated viscraft.h method names to match cpp file.  - Changed release project settings to enable warnings as errors.  - Fixed warning caused by disabling runtime c++ exceptions

--- src/main.h
+++ src/main.h
@@ -7,7 +7,7 @@
 /**
 	Advanced memory leek detection
 */
-#if defined(_DEBUG)
+#ifdef _DEBUG
 	#include <crtdbg.h>
 	#define new new( _NORMAL_BLOCK, __FILE__, __LINE__)
 	#pragma warning(disable : 4345)
