081125a475a415706a5664090cad334b51576ae4
Sam Oates <sam@samoatesgames.com>
2012-10-14 12:19:51 +0000

    Updated the terrain class comments
    THe terrain class was missing all its comments, guessing I rushed that code.

--- src/cterrain.h
+++ src/cterrain.h
@@ -7,10 +7,11 @@
 
 class CTerrain {
 private:
+	//! The vertex type deceleration
 	struct VertexType
 	{
-		D3DXVECTOR3 position;
-		D3DXVECTOR4 color;
+		D3DXVECTOR3 position;									//!< The position of the vertex
+		D3DXVECTOR4 color;										//!< The color of the vertex
 	};
 
 private:
@@ -20,19 +21,20 @@ private:
 	unsigned int			m_vertexCount;						//!< The number of verticies
 	unsigned int			m_indexCount;						//!< The number of indecies
 
-	ID3D11Buffer			*m_vertexBuffer;					//!< 
-	ID3D11Buffer			*m_indexBuffer;						//!< 
+	ID3D11Buffer			*m_vertexBuffer;					//!< Terrain D3D11 vertex buffer
+	ID3D11Buffer			*m_indexBuffer;						//!< Terrain D3D11 index buffer
 
 private:
 							//! Initialize the terrains vertex buffers
 	const bool				InitializeBuffers();
 
-	void					AddVerticie(
-								VertexType *vertices,
-								unsigned long *indices,
-								const int x,
-								const int z,
-								unsigned int &index
+							//! Add a vertex at a given x,z coordinate
+	void					AddVertex(
+								VertexType *vertices,			//!< A pointer to the vertex array
+								unsigned long *indices,			//!< A pointer to the index array
+								const int x,					//!< The x coordinate of the vertex
+								const int z,					//!< The z coordinate of the vertex
+								unsigned int &index				//!< The index into the arrays this vertex represents
 							);
 
 public:
@@ -42,20 +44,20 @@ public:
 							//! Class destructor
 							~CTerrain();
 
-							//! 
+							//! Create a flat terrain
 	bool					Create( 
 								CRenderer *renderer				//!< Pointer to the render
 							);
 
-							//! 
+							//! Release any resources allocated by the terrain class
 	void					Release();
 
-							//! 
+							//! Update the terrain for the D3D11 renderer
 	void					Update();
 
-							//! 
-	inline unsigned int		GetIndexCount()
-	{
-		return m_indexCount;
-	}
+							//! Returns the index count
+	inline unsigned int		GetIndexCount() const
+							{
+								return m_indexCount;
+							}
 };
