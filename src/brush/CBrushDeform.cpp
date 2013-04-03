631e2c5b3176bc12e122faff1b3e7624b5ec8c89
samoatesgames <samoatesgames@0aab44fd-6b68-406f-af70-1c002fc39f85>
2013-04-03 14:23:24 +0000

    Updating other brushes to work with kinect

--- src/brush/CBrushDeform.cpp
+++ src/brush/CBrushDeform.cpp
@@ -52,5 +52,32 @@ void CBrushDeform::Apply(
 	CTerrain *terrain				//!< The terrain object we want to apply the brush too 
 	)
 {
+	if (!kinect->GetHandState() == HandState::ClosedFist)
+		return;
 
+	const D3DXVECTOR2 mousePos = kinect->GetHandPosition();
+	const float moveAmount = ((mousePos.y - gizmo->DragData().lastY) * 0.025f) * m_strength;
+
+	if (moveAmount == 0.0f)
+		return;
+
+	for (int xOffset = -m_size; xOffset <= m_size; ++xOffset)
+	{
+		for (int zOffset = -m_size; zOffset <= m_size; ++zOffset)
+		{
+			HeightMap *hmap = terrain->GetTerrainVertexAt(gizmo->Position().x + xOffset, gizmo->Position().z + zOffset);
+
+			float scale = 1;
+			if (xOffset < 0) scale += -xOffset; else scale += xOffset;
+			if (zOffset < 0) scale += -zOffset; else scale += zOffset;
+			scale *= 0.75f;
+
+			const float scaledMovedAmount = moveAmount / (scale);
+
+			hmap->position.y -= scaledMovedAmount;
+		}
+	}
+
+	terrain->UpdateHeightMap();
+	gizmo->DragData().lastY = mousePos.y;
 }
